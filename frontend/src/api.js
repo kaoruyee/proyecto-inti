c6ab412877860a04c306a4befebafcf453bd4f67
kaoruyee <emilio.zuniga100@gmail.com>
2026-05-03 04:41:07 -0400

    conexion a render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL = "https://inti-backend-xnq4.onrender.com/api";
 
 /* FETCH CON TOKEN AUTOMÁTICO */
 export const authFetch = (endpoint, options = {}) => {
