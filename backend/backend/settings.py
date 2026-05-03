fe50eba9f83a08c4548bcfec44d72b860a03883f
kaoruyee <emilio.zuniga100@gmail.com>
2026-05-03 03:29:00 -0400

    static files config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
